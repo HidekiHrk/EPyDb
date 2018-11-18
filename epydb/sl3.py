1c6a36f768a282306e7c09a6ca4463e536bf08d7
HidekiHrk <hidekihiroki123@gmail.com>
2018-11-18 16:54:01 -0300

    .

--- epydb/sl3.py
+++ epydb/sl3.py
@@ -1,4 +1,5 @@
 import sqlite3
+from collections import namedtuple
 
 #	Error Classes #
 class TableError(Exception):
@@ -24,32 +25,77 @@ class Sl3:
 	
 	# Script to create tables easily at Will #
 	def add_table(self, tablename, **kwargs):
-		self.__cursor.execute(
-			"SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?", 
-			(tablename,)
-		)
+		ignore = kwargs.get('ignore')
+		if type(ignore) != bool:
+			ignore = False
+		elif isinstance(ignore, bool):
+			del kwargs['ignore']
+		elif ignore == None:
+			ignore = False
 		supported_types = ['TEXT', 'INT']
 		table_columns_string = []
+		if len(kwargs) == 0:
+			raise TypeError('Table must have at least 1 column, but 0 were given.')
 		for arg in kwargs:
-			argtype = kwargs.get('arg')
+			argtype = kwargs.get(arg)
 			if argtype.upper() in supported_types:
 				table_columns_string.append('%s %s' % (arg, argtype.upper()))
 				continue
 			else:
-				raise ColumnTypeError('Column type must be one of these ( %s ), not %s' % 
+				raise ColumnTypeError('Column type must be one of these ( %s ), not \'%s\'' % 
 					(', '.join(supported_types), argtype)
 				)
 		else:
 			table_columns_string = ' '.join(table_columns_string)
-		if self.__cursor.fetchall()[0][0] == 0:
+		self.__cursor.execute(
+			"SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?", 
+			(tablename,)
+		)
+		if self.__cursor.fetchone()[0] == 0:
 			self.__cursor.execute("CREATE TABLE IF NOT EXISTS %s (%s)" % (tablename, table_columns_string))
 			self.__conn.commit()
-			return True
 		else:
-			raise TableError('Table %s already exists.' % (tablename))
+			if not ignore:
+				raise TableError('Table %s already exists.' % tablename)
+		return self
 	
+	# Script to delete tables #
+	def del_table(self, tablename, ignore=False):
+		self.__cursor.execute(
+			"SELECT COUNT(*) FROM sqlite_master WHERE type'table' AND name=?",
+			(tablename,)
+		)
+		if self.__cursor.fetchall()[0][0] > 0:
+			self.__cursor.execute("DROP TABLE %s" % tablename)
+			self.__cursor.commit()
+		else:
+			if not ignore:
+				raise TableError('Table %s not exists.' % tablename)
+		return self
+
+	#def create_row(self):
+
+	def list_tables(self):
+		self.__cursor.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name")
+		table_tuple = namedtuple('table', 'name cols')
+		column_tuple = namedtuple('column', 'id name type notnull dvalue pk')
+		table_list = tuple(map(lambda x: x[0], self.__cursor.fetchall()))
+		table_tuple_list = []
+		for tablename in table_list:
+			self.__cursor.execute("PRAGMA table_info(%s)" % tablename)
+			cols = tuple(map(lambda z: column_tuple(*z), self.__cursor.fetchall()))
+			table_tuple_list.append(table_tuple(tablename, cols))
+		return tuple(table_tuple_list)
+
 	# Removing number of instances and close database connection when deleted #
 	def __del__(self):
 		if Sl3.connections > 0:
 			self.__conn.close()
-			connections -= 1
+			Sl3.connections -= 1
+
+if __name__ == '__main__':
+	conct = Sl3()
+	print(Sl3.connections)
+	print(conct.list_tables())
+	conct.add_table('hideki', opa='TEXT', ignore=True)
+	print(conct.list_tables())
