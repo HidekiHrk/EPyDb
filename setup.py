d9963eb1c4d8b7ba35e85388eaea24fdc66e5c65
HidekiHrk <hidekihiroki123@gmail.com>
2018-11-22 17:26:58 -0300

    .

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPL-3 License',   # Again, pick a license
+    'License :: OSI Approved :: GPL-3 License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
