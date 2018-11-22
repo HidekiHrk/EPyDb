4d758d45f9addea556907cc8e3797d6d917fce2b
HidekiHrk <hidekihiroki123@gmail.com>
2018-11-22 17:26:15 -0300

    .

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from distutils.core import setup
-
 setup(
-	name='EPyDb',
-	version='0.1dev',
-	packages=['epydb'],
-	license='GPL-3.0',
+  name='EPyDb',
+  packages = ['epydb'],
+  version = '0.1dev',
+  license='GPL-3.0',
+  description = 'Enhanced Python database generator',
+  author = 'HidekiHrk',
+  author_email = 'hidekihiroki123@gmail.com',
+  url = 'https://github.com/HidekiHrk/EPyDb',
+  download_url = 'https://github.com/user/reponame/archive/v_01dev.tar.gz',
+  keywords = ['python', 'db', 'database', 'better'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GPL-3 License',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
